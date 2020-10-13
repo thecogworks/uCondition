85e232bd194c4d5caf0df5bdba737c08a30d7e0a
Adrian Ochmann <adrian.ochmann@outlook.com>
2020-10-13 12:01:05 +0200

    Added register predicate extension methods

---
+++ uCondition.Core/Extensions/CompositionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using uCondition.Models;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+
+namespace uCondition.Core.Extensions
+{
+    public static class CompositionExtensions
+    {
+        public static Composition RegisterPredicate<TPredicate>(this Composition composition, Lifetime lifetime = Lifetime.Singleton)
+            where TPredicate : Predicate
+        {
+            composition.Register<TPredicate>(lifetime);
+            return composition;
+        }
+
+        public static Composition RegisterPredicate<TPredicate>(this Composition composition, TPredicate instance, Lifetime lifetime = Lifetime.Singleton)
+            where TPredicate : Predicate
+        {
+            composition.Register(_ => instance, lifetime);
+            return composition;
+        }
+    }
+}
